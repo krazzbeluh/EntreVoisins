852fc2161792d6bdc7da0dd25ddcc49642b829b3
Paul Leclerc <paulleclerc@icloud.com>
2020-02-17 17:14:35 +0100

    Implemented UI functionnality

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeighbourDetailActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeighbourDetailActivity.java
@@ -1,9 +1,13 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
@@ -18,7 +22,13 @@ public class NeighbourDetailActivity extends AppCompatActivity {
 
     private Neighbour mNeighbour;
 
+    private ImageButton mBackButton;
     private ImageView mAvatar;
+    private FloatingActionButton mFavButton;
+    private TextView mAdress;
+    private TextView mPhone;
+    private TextView mWebProfile;
+    private TextView mDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +38,40 @@ public class NeighbourDetailActivity extends AppCompatActivity {
         Intent intent = this.getIntent();
         Bundle bundle = intent.getBundleExtra(BUNDLE_IDENTIFIER);
         this.mNeighbour = (Neighbour) bundle.getSerializable(SERIALIZABLE_IDENTIFIER);
+
+        this.mBackButton = findViewById(R.id.image_details_back_arrow);
         this.mAvatar = findViewById(R.id.neighbour_avatar_image);
+        this.mFavButton = findViewById(R.id.fav_neighbour_button);
+        this.mAdress = findViewById(R.id.detail_location);
+        this.mPhone = findViewById(R.id.detail_phone);
+        this.mWebProfile = findViewById(R.id.detail_network);
+        this.mDescription = findViewById(R.id.detail_description);
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+
+        toolbar.setTitle(mNeighbour.getName());
+        mAdress.setText(mNeighbour.getAddress());
+        mPhone.setText(mNeighbour.getPhoneNumber());
+        mWebProfile.setText("www.facebook.fr/" + mNeighbour.getName());
+        mDescription.setText(mNeighbour.getAboutMe());
 
         Glide.with(this).load(mNeighbour.getAvatarUrl()).placeholder(R.drawable.ic_account)
                 .apply(RequestOptions.centerCropTransform()).into(mAvatar);
+        updateFavButton();
+
+        mBackButton.setOnClickListener(v -> finish());
+
+        mFavButton.setOnClickListener(v -> {
+            getPreferences(MODE_PRIVATE).edit().putBoolean(String.valueOf(mNeighbour.getId()), !checkFavorite()).apply();
+            updateFavButton();
+        });
+    }
+
+    private void updateFavButton() {
+        mFavButton.setImageResource(checkFavorite() ? R.drawable.ic_star_white_24dp : R.drawable.ic_star_border_white_24dp);
+    }
+
+    private boolean checkFavorite() {
+        return getPreferences(MODE_PRIVATE).getBoolean(String.valueOf(mNeighbour.getId()), false);
     }
 
     public Neighbour getNeighbour() {
